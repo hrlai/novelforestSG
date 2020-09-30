a4557949197aeea819756f38fd44d61fd1e09fbc
Hao Ran <hrlai.ecology@gmail.com>
2020-10-01 11:15:26 +1300

    add package documentation

---
+++ R/novelforestSG-package.R
@@ -0,0 +1,18 @@
+#' novelforestSG
+#'
+#' The dataset and model used in Lai et al. (2020)
+#' Decoupled responses of native and exotic tree diversities to
+#' distance from old-growth forest and soil phosphorous in
+#' novel secondary forests. \emph{Applied Vegetation Science}.
+#'
+#' The main component of \code{novelforestSG} is the dataset, which can be accessed with \code{novelforest$data}. See \code{?novelforest} for more details.
+#'
+#' @references
+#' Lai HR, Tan CSY, Neo L, Kee CY, Yee ATK, Tan HTW, Chong KY. (2020)
+#' Decoupled responses of native and exotic tree diversities to distance from old-growth forest
+#' and soil phosphorous in novel secondary forests. *Applied Vegetation Science*
+#'
+#' @docType package
+#' @name novelforestSG
+#' @import brms
+NULL
