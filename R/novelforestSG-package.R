0d9d4e352b6383fd9a0623265547230a7bff3094
Hao Ran <hrlai.ecology@gmail.com>
2020-10-01 16:13:53 +1300

    minor tweaks

--- R/novelforestSG-package.R
+++ R/novelforestSG-package.R
@@ -7,6 +7,12 @@
 #'
 #' The main component of \code{novelforestSG} is the dataset, which can be accessed with \code{novelforest$data}. See \code{?novelforest} for more details.
 #'
+#' @author
+#' Hao Ran Lai, \email{hrlai.ecology@gmail.com} \cr
+#' Kwek Yan Chong, \email{kwek@u.nus.edu} \cr
+#' Alex Thiam Koon Yee \cr
+#' Maintainer: Hao Ran Lai, \email{hrlai.ecology@gmail.com}
+#'
 #' @references
 #' Lai HR, Tan CSY, Neo L, Kee CY, Yee ATK, Tan HTW, Chong KY. (2020)
 #' Decoupled responses of native and exotic tree diversities to distance from old-growth forest
