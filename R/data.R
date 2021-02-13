d20a33a6702358f1c2d10a93a77c10bf23c6fc93
Hao Ran Lai <hrlai.ecology@gmail.com>
2021-02-14 10:18:23 +1300

    update page number

--- R/data.R
+++ R/data.R
@@ -51,8 +51,8 @@
 #' @references Lai, H.R., Tan, G.S.Y., Neo, L., Kee, C.Y., Yee, A.T.K., Tan, H.T.W.
 #' and Chong, K.Y. (2021) Decoupled responses of native and exotic tree
 #' diversities to distance from old-growth forest and soil phosphorous in
-#' novel secondary forests. *Applied Vegetation Science*.
-#' doi: 10.1111/avsc.12548
+#' novel secondary forests. *Applied Vegetation Science*, 24, e12548.
+#' https://doi.org/10.1111/avsc.12548
 #'
 #' @examples
 #' # To access the dataset
