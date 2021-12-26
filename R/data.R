658f629b71887c4a88a5c9fecb1f63f239604d26
Hao Ran Lai <hrlai.ecology@gmail.com>
2021-12-27 12:21:27 +1300

    update and document raw data

--- R/data.R
+++ R/data.R
@@ -1,47 +1,39 @@
 #' Novel Forest Dataset
 #'
-#' The dataset used in Lai et al. (2021), who examined the diversity--environment
-#' relationships of native and exotic species in two types of post-cultivation novel secondary
-#' forests in Singapore.
+#' The dataset used in Lai et al. (2021), also comprised part of the data in
+#' Neo et al. (2017).
 #'
 #' @docType data
 #'
 #' @usage novelforest_data
 #'
-#' @format A \code{data.frame} containing the following response variables:
+#' @format A \code{data.frame} containing the following variables:
 #' \describe{
-#'   \item{SD_N_0}{first-order native taxonomic diversity, i.e., species richness}
-#'   \item{SD_N_2}{second-order native taxonomic diversity, i.e., inverse Simpson index}
-#'   \item{SD_E_0}{first-order exotic taxonomic diversity}
-#'   \item{SD_E_2}{second-order exotic taxonomic diversity}
-#'   \item{FD_N_0}{first-order native functional diversity}
-#'   \item{FD_N_2}{second-order native functional diversity}
-#'   \item{FD_E_0}{first-order exotic functional diversity}
-#'   \item{FD_E_2}{second-order exotic functional diversity,}
+#'   \item{patch}{Name of forest patch}
+#'   \item{plot}{Name of forest plot within patch}
+#'   \item{UID}{Unique stem identifier}
+#'   \item{species}{Species name following Chong et al. (2011)}
+#'   \item{stem}{Indicator column denoting whether a stem is the main trunk (=1)
+#'   of an individual tree, or otherwise (=0)}
+#'   \item{dbh_2011}{Diameter-at-breast-height (cm) measured in year 2011.}
 #' }
-#' and the following explanatory variables (and measurement units if you backtransform
-#' them using \code{backtransform}):
-#' \describe{
-#'   \item{dist}{Distance to old-growth forests (m)}
-#'   \item{size}{Patch area (km^2)}
-#'   \item{nitrogen}{Total soil nitrogen (mg/kg)}
-#'   \item{phosphorous}{Total extractable soil phosphorous (mg/kg)}
-#'   \item{potassium}{Total extractable soil potassium (mg/kg)}
-#'   \item{patch}{Forest patch ID}
-#' }
+#' See Lai et al. (2021) for more details on data collection.
 #'
-#' Note that all explanatory variables were log-transformed and standardised to zero mean and
-#' unit standard deviations. Use \code{backtransform} to obtain the variables in
-#' their original scales. See Lai et al. (2021) for more details on model building and
-#' data collection.
+#' @references
+#' Chong, K. Y., Tan, H. T. W. and Corlett, R. T. (2011). A summary of the total
+#' vascular plant flora of Singapore. *Gardens' Bulletin Singapore*,
+#' 63, 197-204.
 #'
-#' @seealso backtransform
-#'
-#' @references Lai, H.R., Tan, G.S.Y., Neo, L., Kee, C.Y., Yee, A.T.K., Tan, H.T.W.
+#' Lai, H.R., Tan, G.S.Y., Neo, L., Kee, C.Y., Yee, A.T.K., Tan, H.T.W.
 #' and Chong, K.Y. (2021) Decoupled responses of native and exotic tree
 #' diversities to distance from old-growth forest and soil phosphorous in
 #' novel secondary forests. *Applied Vegetation Science*, 24, e12548.
 #' \doi{10.1111/avsc.12548}
+#'
+#' Neo, L., Yee, A. T. K., Chong, K. Y., Kee, C. Y. and Tan, H. T. W. (2017).
+#' Vascular plant species richness and composition in two types of
+#' post-cultivation tropical secondary forest. *Applied Vegetation Science*,
+#' 20, 692-701. \doi{10.1111/avsc.12322}
 #'
 #' @examples
 #' # To access the dataset
